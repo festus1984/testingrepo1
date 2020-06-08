49860ea2c14764760c97b52e9b74bf351f2ffec7
nfesta <festusnkeng@yahoo.com>
2020-06-07 20:35:31 -0400

    adding changes

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp() {
+ echo 'building the application...'
+}
+
+def TestApp() {
+ echo 'building the application...'
+}
+
+def DeployApp() {
+ echo 'building the application...'
+ echo "deploy version ${params.VERSION}"
+}
+
+return this
